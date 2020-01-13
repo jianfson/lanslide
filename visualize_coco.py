78a95714c6bbddc2cf095223c3786570ef7ad5ab
jianfson <jiangxin_cdut@163.com>
2020-01-13 13:59:38 +0800

    test 174 slide

--- visualize_coco.py
+++ visualize_coco.py
@@ -69,7 +69,8 @@ print('Custom COCO supercategories: \n{}'.format(' '.join(category_names)))
 #exit()
 
 category_ids = example_coco.getCatIds(catNms=['landslide'])
-image_ids = example_coco.getImgIds(catIds=category_ids)
+#image_ids = example_coco.getImgIds(catIds=category_ids)
+image_ids = coco.getImgIds(imgIds = [174])
 image_data = example_coco.loadImgs(image_ids[np.random.randint(0, len(image_ids))])[0]
 #img_size=2000
 #sum_r=0
