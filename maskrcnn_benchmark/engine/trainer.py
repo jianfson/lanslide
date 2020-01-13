842751d964abba65a79223c5b5afcde08202db24
jianfson <jiangxin_cdut@163.com>
2020-01-13 09:42:01 +0800

    add mask rcnn model

---
+++ maskrcnn_benchmark/engine/trainer.py
@@ -0,0 +1,152 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+import datetime
+import logging
+import time
+
+import torch
+import torch.distributed as dist
+
+from maskrcnn_benchmark.utils.comm import get_world_size
+from maskrcnn_benchmark.utils.metric_logger import MetricLogger
+
+import ipdb
+import time
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from torchvision.transforms import transforms
+from torchvision.transforms import ToPILImage
+
+def reduce_loss_dict(loss_dict):
+    """
+    Reduce the loss dictionary from all processes so that process with rank
+    0 has the averaged results. Returns a dict with the same fields as
+    loss_dict, after reduction.
+    """
+    world_size = get_world_size()
+    if world_size < 2:
+        return loss_dict
+    with torch.no_grad():
+        loss_names = []
+        all_losses = []
+        for k, v in loss_dict.items():
+            loss_names.append(k)
+            all_losses.append(v)
+        all_losses = torch.stack(all_losses, dim=0)
+        dist.reduce(all_losses, dst=0)
+        if dist.get_rank() == 0:
+            # only main process gets accumulated, so only divide by
+            # world_size in this case
+            all_losses /= world_size
+        reduced_losses = {k: v for k, v in zip(loss_names, all_losses)}
+    return reduced_losses
+
+
+def do_train(
+    model,
+    data_loader,
+    optimizer,
+    scheduler,
+    checkpointer,
+    device,
+    checkpoint_period,
+    arguments,
+):
+    logger = logging.getLogger("maskrcnn_benchmark.trainer")
+    logger.info("Start training")
+    meters = MetricLogger(delimiter="  ")
+    max_iter = len(data_loader)
+    print(max_iter)
+    start_iter = arguments["iteration"]
+    print(start_iter)
+    model.train()
+    start_training_time = time.time()
+    end = time.time()
+
+    for iteration, (images, targets, _) in enumerate(data_loader, start_iter):
+        data_time = time.time() - end
+        iteration = iteration + 1
+        arguments["iteration"] = iteration
+
+        #ipdb.set_trace()
+        #unloader=transforms.ToPILImage()
+        #showimg = unloader(images.tensors[0])
+        #showimg = np.array(showimg)
+        #bboxs=targets[0].bbox
+        #bboxs = bboxs.numpy()
+        #for bbox in bboxs:
+        #    cv2.rectangle(showimg, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 0, 255), 4)
+        #plt.imshow(showimg)
+        #for mask in targets[0].extra_fields['masks'].polygons:
+        #    poly = mask.polygons
+        #    poly = poly[0]
+        #    #poly = poly.numpy()
+        #    n = len(poly)
+        #    x = []
+        #    y = []
+        #    for i in range(int(n/2)):
+        #        x.append(int(poly[i*2]))
+        #        y.append(int(poly[i*2+1]))
+        #    plt.plot(x,y,color="red",linewidth=2.0)
+        #    #plt.scatter(x, y, color = 'red')
+        #ipdb.set_trace()
+        ##plt.imshow(showimg)
+
+        ##plt.show()
+
+
+        scheduler.step()
+
+        #time.sleep( 1 )
+        images = images.to(device)
+        targets = [target.to(device) for target in targets]
+
+        loss_dict = model(images, targets)
+        losses = sum(loss for loss in loss_dict.values())
+
+        # reduce losses over all GPUs for logging purposes
+        loss_dict_reduced = reduce_loss_dict(loss_dict)
+        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+        meters.update(loss=losses_reduced, **loss_dict_reduced)
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+        batch_time = time.time() - end
+        end = time.time()
+        meters.update(time=batch_time, data=data_time)
+
+        eta_seconds = meters.time.global_avg * (max_iter - iteration)
+        eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+
+        if iteration % 20 == 0 or iteration == max_iter:
+            logger.info(
+                meters.delimiter.join(
+                    [
+                        "eta: {eta}",
+                        "iter: {iter}",
+                        "{meters}",
+                        "lr: {lr:.6f}",
+                        "max mem: {memory:.0f}",
+                    ]
+                ).format(
+                    eta=eta_string,
+                    iter=iteration,
+                    meters=str(meters),
+                    lr=optimizer.param_groups[0]["lr"],
+                    memory=torch.cuda.max_memory_allocated() / 1024.0 / 1024.0,
+                )
+            )
+        if iteration % checkpoint_period == 0:
+            checkpointer.save("model_{:07d}".format(iteration), **arguments)
+
+    checkpointer.save("model_{:07d}".format(iteration), **arguments)
+    total_training_time = time.time() - start_training_time
+    total_time_str = str(datetime.timedelta(seconds=total_training_time))
+    logger.info(
+        "Total training time: {} ({:.4f} s / it)".format(
+            total_time_str, total_training_time / (max_iter)
+        )
+    )
--- visualize_coco.py
+++ visualize_coco.py
@@ -6,12 +6,57 @@ import skimage.io as io
 import matplotlib.pyplot as plt
 import pylab
 import cv2
+import ipdb
+import pycocotools.mask as maskutil
+from skimage import measure,draw,data
 
-image_directory = '/home/harman-jx/work/landslide/test/jpeg/'
-annotation_file = '/home/harman-jx/work/landslide/test/landslide_val_google_20191115.json'
+
+def close_contour(contour):
+    if not np.array_equal(contour[0], contour[-1]):
+        contour = np.vstack((contour, contour[0]))
+    return contour
+
+def binary_mask_to_polygon(binary_mask, tolerance=0):
+    """Converts a binary mask to COCO polygon representation
+    Args:
+        binary_mask: a 2D binary numpy array where '1's represent the object
+        tolerance: Maximum distance from original points of polygon to approximated
+            polygonal chain. If tolerance is 0, the original coordinate array is returned.
+    """
+    polygons = []
+    # pad mask to close contours of shapes which start and end at an edge
+    padded_binary_mask = np.pad(binary_mask, pad_width=1, mode='constant', constant_values=0)
+    contours = measure.find_contours(padded_binary_mask, 0.5)
+    contours = np.subtract(contours, 1)
+    for contour in contours:
+        contour = close_contour(contour)
+        contour = measure.approximate_polygon(contour, tolerance)
+        if len(contour) < 3:
+            continue
+        contour = np.flip(contour, axis=1)
+        segmentation = contour.ravel().tolist()
+        # after padding and subtracting 1 we may get -0.5 points in our segmentation
+        segmentation = [0 if i < 0 else i for i in segmentation]
+        polygons.append(segmentation)
+
+    return polygons
+
+#image_directory = '/home/harman-jx/work/landslide/test/jpeg/'
+#annotation_file = '/home/harman-jx/work/landslide/test/landslide_val_google_20191115.json'
+#image_directory = '/media/jiangxin/data/lanslide/train/jpeg/'
+#annotation_file = '/media/jiangxin/data/lanslide/train/landslide_train_google_20191115.json'
+image_directory = '/media/jiangxin/data/lanslide/test/jpeg/'
+annotation_file = '/media/jiangxin/data/lanslide/annotations/landslide_val_google_20191115.json'
+
+bbox_test_file = '/media/jiangxin/data/lanslide/models/inference/coco_2014_minival/bbox.json'
+seg_test_file = '/media/jiangxin/data/lanslide/models/inference/coco_2014_minival/segm.json'
 
 example_coco = COCO(annotation_file)
-
+bbox_coco=example_coco.loadRes(bbox_test_file)
+seg_coco = example_coco.loadRes(seg_test_file)
+#bbox_coco = COCO(bbox_test_file)
+#seg_coco = COCO(seg_test_file)
+#ipdb.set_trace()
 categories = example_coco.loadCats(example_coco.getCatIds())
 category_names = [category['name'] for category in categories]
 print('Custom COCO categories: \n{}\n'.format(' '.join(category_names)))
@@ -26,22 +71,55 @@ print('Custom COCO supercategories: \n{}'.format(' '.join(category_names)))
 category_ids = example_coco.getCatIds(catNms=['landslide'])
 image_ids = example_coco.getImgIds(catIds=category_ids)
 image_data = example_coco.loadImgs(image_ids[np.random.randint(0, len(image_ids))])[0]
+#img_size=2000
+#sum_r=0
+#sum_g=0
+#sum_b=0
+#count=0
 #for image_id in image_ids:
 #    image_data = example_coco.loadImgs(image_id)[0]
-#    print(image_data)
-
+#    #image = io.imread(image_directory + image_data['file_name'])
+#    img=cv2.imread(image_directory + image_data['file_name'])
+#    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+#    img=cv2.resize(img,(img_size,img_size))
+#    sum_r=sum_r+img[:,:,0].mean()
+#    sum_g=sum_g+img[:,:,1].mean()
+#    sum_b=sum_b+img[:,:,2].mean()
+#    count=count+1
+##    annotation_ids = example_coco.getAnnIds(imgIds=image_id, catIds=category_ids, iscrowd=None)
+##    annotations = example_coco.loadAnns(annotation_ids)
+##    for annotation in annotations:
+##        #if annotation['iscrowd'] == 1:
+##        print('iscrowd: ' + str(annotation['iscrowd']))
+#sum_r=sum_r/count
+#sum_g=sum_g/count
+#sum_b=sum_b/count
+#img_mean=[sum_r,sum_g,sum_b]
+#print (img_mean)
 #exit()
 
 # load and display instance annotations
+#image = io.imread(image_directory + 'J48E022011_wow_818_zx.jpeg')
+plt.figure()
 image = io.imread(image_directory + image_data['file_name'])
-plt.imshow(image); plt.axis('off')
-pylab.rcParams['figure.figsize'] = (8.0, 10.0)
+#plt.imshow(image); plt.axis('off')
+#pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 annotation_ids = example_coco.getAnnIds(imgIds=image_data['id'], catIds=category_ids, iscrowd=None)
 annotations = example_coco.loadAnns(annotation_ids)
+
+bbox_annotation_ids = bbox_coco.getAnnIds(imgIds=image_data['id'], catIds=category_ids, iscrowd=None)
+bbox_annotations = bbox_coco.loadAnns(bbox_annotation_ids)
+
+seg_annotation_ids = seg_coco.getAnnIds(imgIds=image_data['id'], catIds=category_ids, iscrowd=None)
+seg_annotations = seg_coco.loadAnns(seg_annotation_ids)
+ipdb.set_trace()
 masks = []
 for annotation in annotations:
+    bbox = annotation['bbox']
+    cv2.rectangle(image, (int(bbox[0]), int(bbox[1])), (int(bbox[0]+bbox[2]), int(bbox[1]+bbox[3])), (0, 0, 255), 2)
     print(annotation)
     mask = example_coco.annToMask(annotation)
+#area = np.sum(np.greator(mask,0))
     masks.append(mask)
     segmentation = annotation['segmentation']
     n = len(segmentation[0])
@@ -50,12 +128,40 @@ for annotation in annotations:
     for i in range(int(n/2)):
         x.append(segmentation[0][i*2])
         y.append(segmentation[0][i*2+1])
-    plt.scatter(x, y, color = 'red')
+#plt.scatter(x, y, color = 'red')
+plt.subplot(1,2,1)
+plt.imshow(image); plt.axis('off')
+plt.title('Ground Truth')
+pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 example_coco.showAnns(annotations)
+#seg_coco.showAnns(seg_annotations)
+#plt.show()
+
+
+image2 = io.imread(image_directory + image_data['file_name'])
+for annotation in bbox_annotations:
+    bbox = annotation['bbox']
+    cv2.rectangle(image2, (int(bbox[0]), int(bbox[1])), (int(bbox[0]+bbox[2]), int(bbox[1]+bbox[3])), (255, 0, 0), 2)
+plt.subplot(1,2,2)
+plt.imshow(image2); plt.axis('off')
+plt.title('prediction')
+pylab.rcParams['figure.figsize'] = (8.0, 10.0)
+#example_coco.showAnns(annotations)
+seg_coco.showAnns(seg_annotations)
 plt.show()
+exit()
+#label = np.zeros_like(image, dtype='uint8')
+label = masks[0]
 for mask in masks:
-#cv2.imwrite("temp.jpeg", mask)
-#temp = cv2.imread("temp.jpeg", cv2.IMREAD_GRAYSCALE)
+    #cv2.imwrite("temp.jpeg", mask)
+    #temp = cv2.imread("temp.jpeg", cv2.IMREAD_GRAYSCALE)
+#label = np.zeros_like(im, dtype='uint8')
+#im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+#ret, im = cv2.threshold(im, 220, 255, cv2.THRESH_BINARY)
     masked = cv2.add(image, np.zeros(np.shape(image), dtype=np.uint8), mask = mask)
-    plt.imshow(masked)
-    plt.show()
+    for i in range(mask.shape[0]):
+        for j in range(mask.shape[1]):
+            if mask[i, j] != 0:
+                label[i, j] = 1
+plt.imshow(label)
+plt.show()
